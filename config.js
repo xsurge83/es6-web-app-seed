bc5c4bf926287de12384d86f1a75ee8d71a2d727
xsurge83 <xsurge83@gmail.com>
2015-04-24 16:56:49 -0700

    removed unused reference

--- config.js
+++ config.js
@@ -2,8 +2,7 @@ System.config({
   "paths": {
     "*": "*.js",
     "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js",
-    "aurelia-skeleton-navigation/*": "lib/*.js"
+    "npm:*": "jspm_packages/npm/*.js"
   }
 });
 
